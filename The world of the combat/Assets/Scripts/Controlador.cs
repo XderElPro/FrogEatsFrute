209667a2a5067af05716bf34e13fb5bd57aa59d9
xderelpro <fharosantos@gmail.com>
2021-02-28 12:18:40 +0100

    Game added

---
+++ The world of the combat/Assets/Scripts/Controlador.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controlador : MonoBehaviour
+{
+    public void CambiarEscena(string nombre)
+    {
+        print("Cambiando a la escena " + nombre);
+    }
+    public void Salir()
+    {
+        print("Saliendo del juego");
+    }
+
+}
+
---
+++ The world of the combat/Assets/Scripts/FruitsCollected.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitsCollected : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            GetComponent<SpriteRenderer>().enabled = false;
+            gameObject.transform.GetChild(0).gameObject.SetActive(true);
+
+            FindObjectOfType<FruitManager>().AllFruitsCollected();
+
+            Destroy(gameObject, 0.5f);
+
+        }
+
+        
+
+    }
+
+}
